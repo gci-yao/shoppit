7a1f351859a7b6a9a5d3efeb36167af93b84e2eb
charles <yaogci2002@gmail.com>
2025-04-25 17:56:18 +0000

    allowed origin

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -83,7 +83,8 @@ INSTALLED_APPS = [
 # pip install django-cors-headers
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://blaato.onrender.com"
 ] 
 
 MIDDLEWARE = [
