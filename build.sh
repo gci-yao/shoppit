823a66fb4d8feddefb2f05f292620af50f04f939
gci-yao <147353955+gci-yao@users.noreply.github.com>
2025-04-25 14:22:26 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit  #exit on error
 
 pip install -r requirements.txt
- python manage.py collectstatic --no-input
- python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
