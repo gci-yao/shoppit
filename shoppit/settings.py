984b7fe98fb1356be30aeff21d5c220fd3b20037
charles <yaogci2002@gmail.com>
2025-04-26 02:20:14 +0000

    last commit 12

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -112,10 +112,10 @@ CORS_ALLOW_HEADERS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', #ce lien aussi a ajouter en 1er !
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
