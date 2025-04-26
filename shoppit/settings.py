ee5b4a3b43b139d29acc0130c0cfed9e99f38dfe
charles <yaogci2002@gmail.com>
2025-04-26 14:10:18 +0000

    last commit 22

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -79,27 +79,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
-# Autorisez les méthodes et en-têtes nécessaires
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
 
 
 
