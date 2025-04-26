6bbdd00a1c556ecd312b72b6c368d74b7bffc3de
charles <yaogci2002@gmail.com>
2025-04-26 13:07:20 +0000

    last commit 17

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -80,6 +80,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ] 
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
 # Autorisez les méthodes et en-têtes nécessaires
